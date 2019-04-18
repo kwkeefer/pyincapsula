f2ca1972ca1ac17f19e563cd55ea6c282e4a6930
kkeefer <kyle.keefer@experianinteractive.com>
2019-04-18 11:15:16 -0700

    add site modifications

--- incapsula/addSite.py
+++ incapsula/addSite.py
@@ -8,6 +8,13 @@ instructions to finnish verifing and setting up the site.
  account_id -- sub-account to associate the site with
  api_id -- API ID to use (Default: enviroment variable)
  api_key -- API KEY to use (Default: enviroment variable)
+ ref_id -- Customer specific identifier for this operation.	
+ site_ip -- Manually set the web server IP/CNAME.	
+ force_ssl -- If this value is "true", manually set the site to support SSL. This option is only available for sites with manually configured IP/CNAME and for specific accounts.	
+ naked_domain_san -- Use “true” to add the naked domain SAN to a www site’s SSL certificate. Default value: true	
+ wildcard_san -- Use “true” to add the wildcard SAN or “false” to add the full domain SAN to the site’s SSL certificate. Default value: true	
+ log_level -- Available only for Enterprise Plan customers that purchased the Logs Integration SKU. Sets the log reporting level for the site. Options are “full”, “security”, “none” and "default"	
+ logs_account_id -- Available only for Enterprise Plan customers that purchased the Logs Integration SKU. Numeric identifier of the account that purchased the logs integration SKU and which collects the logs. If not specified, operation will be performed on the account identified by the authentication parameters	
 """
 
 import os
@@ -19,7 +26,9 @@ api_endpoint = 'https://my.incapsula.com/api/'
 
 def addSite(
         domain, account_id, api_id=os.environ.get('API_ID'),
-        api_key=os.environ.get('API_KEY')):
+        api_key=os.environ.get('API_KEY'), ref_id=None, site_ip=None, 
+        force_ssl=None, naked_domain_san=None, wildcard_san=None, 
+        log_level=None, logs_account_id=None):
     url= api_endpoint + 'prov/v1/sites/add'
     try:
         payload = {
@@ -29,6 +38,22 @@ def addSite(
             'account_id':account_id,
             'send_site_setup_emails':'true'
         }
+
+        if ref_id is not None:
+            payload['ref_id'] = ref_id
+        if site_ip is not None:
+            payload['site_ip'] = site_ip
+        if force_ssl is not None:
+            payload['force_ssl'] = force_ssl
+        if naked_domain_san is not None:
+            payload['naked_domain_san'] = naked_domain_san
+        if wildcard_san is not None:
+            payload['wildcard_san'] = wildcard_san
+        if log_level is not None:
+            payload['log_level'] = log_level
+        if logs_account_id is not None:
+            payload['logs_account_id'] = logs_account_id
+
         r = requests.post(url, data=payload)
         return r.text
     except Exception as error:
